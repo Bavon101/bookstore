b0472f03f8cc39df89241c47d7bbd4351da7750d
Bavon101 <bavonfate@gmail.com>
2022-03-29 21:33:50 +0300

    Cover the root app with a redux provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Books from './components/pages/Books';
 import Categories from './components/pages/Categories';
+import store from './redux/configureStore';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 
