ce2f8c54dd6af3bac3b1733af33ed6199e168223
Bavon101 <bavonfate@gmail.com>
2022-03-29 21:31:40 +0300

    Consume the status checker reducer

--- src/components/pages/Categories.js
+++ src/components/pages/Categories.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
 
 export default function Categories() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button type="submit" className="status-btn">
+      <button type="submit" className="status-btn" onClick={() => dispatch(checkStatus())}>
         Check status
       </button>
     </div>
