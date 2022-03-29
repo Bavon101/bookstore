618aa079d5de2c6c5a2eb55e4b89b272939d8356
Bavon101 <bavonfate@gmail.com>
2022-03-29 21:31:06 +0300

    Dynamically display books via the bookReducer

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookForm from '../BookForm';
 import Book from '../Book';
 
 export default function Books() {
+  const booksList = useSelector((state) => state.bookReducer);
   return (
     <div>
-      <Book title="The Hunger Games" author="Suzanne Collins" />
-      <Book title="Dune" author="Frank Herbert" />
-      <Book title="Capital in the Twenty-First Century" author="Suzanne Collins" />
+      <div>
+        {
+          booksList.length > 0
+            ? booksList.map(
+              (book) => <Book key={book.id} title={book.title} author={book.author} id={book.id} />,
+            )
+            : <h3> There are no available titles, please add one</h3>
+      }
+      </div>
       <hr />
       <BookForm />
     </div>
