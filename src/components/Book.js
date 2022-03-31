87dc44254f99c6c7897bfbbde814350d08b20def
Bavon101 <bavonfate@gmail.com>
2022-03-31 10:58:43 +0300

    Cosue the API calls on the components

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import Vr from './Vr';
-import { removedBook } from '../redux/books/books';
+import apiConstants from '../api/constants';
+import { deleteBook } from '../api/data_fetch';
 
 function Book(props) {
   const { title, author, id } = props;
   const dispatch = useDispatch();
+  const { baseUrl, app } = apiConstants;
+  const url = `${baseUrl}/apps/${app}/books/${id}`;
   return (
     <div className="book">
       <div>
@@ -18,7 +21,7 @@ function Book(props) {
             Comment
           </button>
           <Vr />
-          <button type="submit" onClick={() => dispatch(removedBook(id))}>
+          <button type="submit" onClick={() => dispatch(deleteBook(url, id))}>
             Remove
           </button>
           <Vr />
@@ -34,7 +37,7 @@ function Book(props) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
