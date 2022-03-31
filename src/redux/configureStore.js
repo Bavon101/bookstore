f3d7eaf091947a7a6d7a5cba1008e242459d5eaf
Bavon101 <bavonfate@gmail.com>
2022-03-31 10:59:34 +0300

    Add more redux api related actions for statuses

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
@@ -9,6 +10,6 @@ const rootReducer = combineReducers(
   },
 );
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
